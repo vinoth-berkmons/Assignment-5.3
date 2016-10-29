43ecb9e5f27a1defec25c0c04e2e7c635d81323d
vinoth-berkmons <vinothberkmons@gmail.com>
2016-10-29 10:38:18 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+var employee = {
+
+    name: "Berks",
+    age: 27,
+    salary: "$110K",
+    address: {
+        city: "Canada",
+        state: "Washingto",
+        zip_code: 604
+    }
+};
+
+console.log(employee.name + " , " + employee.age + " , " + employee.salary + " , " + employee.address.city + " , " + employee.address.state + " , " + employee.address.zip_code);
